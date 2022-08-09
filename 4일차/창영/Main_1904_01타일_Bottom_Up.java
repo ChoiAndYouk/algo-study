52f658e2ecf6ad056475d92ccb04777ec0c54c33
chang0-0 <bigyoung8375@naver.com>
2022-08-09 17:01:01 +0900

    complete

--- 4일차/창영/Main_1904_01타일_Bottom_Up.java
+++ 4일차/창영/Main_1904_01타일_Bottom_Up.java
@@ -1,8 +1,7 @@
-import java.util.*;
 import java.io.*;
 
 // https://www.acmicpc.net/problem/1904
-public class Main_1904_01타일 {
+public class Main_1904_01타일_Bottom_Up {
     private static final int MOD = 15746;
     static int memo[] = new int[1000001];
 
@@ -14,12 +13,12 @@ public class Main_1904_01타일 {
         memo[0] = 0;
         memo[1] = 1;
         memo[2] = 2;
-
-        System.out.println(DP(N));
+        System.out.print(DP(N));
     } // End of main
 
+    // DP Bottom-Up (
     private static int DP(int N) {
-        for(int i=3; i<N+1; i++) {
+        for(int i=3; i<=N; i++) {
             memo[i] = (memo[i-1] + memo[i-2]) % MOD;
         }
 
